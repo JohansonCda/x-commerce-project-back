40c3e908cf57e0240884c98c6088e3bf42ece068
JohansonCda <jcv9806@hotmail.com>
2025-05-23 00:11:51 -0500

    Create backend project and scafolding

---
+++ src/app.py
@@ -0,0 +1,9 @@
+from aclimate_v3_orm import create_tables,MngCountryService,CountryCreate
+
+
+# service = MngCountryService()
+# country = CountryCreate(name="Barrancabermeja",iso2='BM')
+# service.create(obj_in=country)
+arra = [1,2,3]
+for a in  arra:
+    print(a)
