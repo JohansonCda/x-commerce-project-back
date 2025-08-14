86ae5388e4f9c52eac2a9f630e3d5106db48c9cd
santiago123x <santiago_123x@hotmail.com>
2025-08-13 19:15:20 -0500

    Add Category model and controller; enhance Product model and controller with category relations

--- app/orm/schemas/__init__.py
+++ app/orm/schemas/__init__.py
@@ -1,8 +1,12 @@
 from .product_schema import ProductCreate, ProductUpdate, ProductRead
+from .category_schema import CategoryCreate, CategoryRead, CategoryUpdate
 
 
 __all__ = [
     "ProductCreate",
     "ProductUpdate",
     "ProductRead",
+    "CategoryCreate",
+    "CategoryRead",
+    "CategoryUpdate"
 ]
