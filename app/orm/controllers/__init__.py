0d85719d4f53cb8214ce4d681565b42eb59bd8f1
santiago123x <santiago_123x@hotmail.com>
2025-08-22 22:48:35 -0500

    Update __init__.py files to include ProductImage and Order schemas/controllers in exports

--- app/orm/controllers/__init__.py
+++ app/orm/controllers/__init__.py
@@ -3,6 +3,10 @@ from .product_controller import ProductController
 from .category_controller import CategoryController
 from .payment_controller import PaymentController
 from .status_controller import StatusController
+from .product_image_controller import ProductImageController
+from .order_controller import OrderController
+from .user_controller import UserController
+from .order_detail_controller import OrderDetailController
 
 
 __all__ = [
@@ -10,5 +14,9 @@ __all__ = [
     "ProductController",
     "CategoryController",
     "PaymentController",
-    "StatusController"
+    "StatusController",
+    "ProductImageController",
+    "OrderController",
+    "UserController",
+    "OrderDetailController"
 ]
--- app/orm/schemas/__init__.py
+++ app/orm/schemas/__init__.py
@@ -3,6 +3,9 @@ from .category_schema import CategoryCreate, CategoryRead, CategoryUpdate
 from .user_schema import UserCreate, UserRead, UserUpdate
 from .status_schema import StatusCreate, StatusRead, StatusUpdate
 from .payment_schema import PaymentCreate, PaymentRead, PaymentUpdate
+from .product_image_schema import ProductImageRead, ProductImageCreate, ProductImageUpdate
+from .order_schema import OrderCreate, OrderRead, OrderUpdate
+from .order_detail_schema import OrderDetailCreate, OrderDetailRead, OrderDetailUpdate
 
 
 __all__ = [
@@ -20,5 +23,14 @@ __all__ = [
     "StatusUpdate",
     "PaymentCreate",
     "PaymentRead",
-    "PaymentUpdate"
+    "PaymentUpdate",
+    "ProductImageCreate",
+    "ProductImageRead",
+    "ProductImageUpdate",
+    "OrderCreate",
+    "OrderRead",
+    "OrderUpdate",
+    "OrderDetailCreate",
+    "OrderDetailRead",
+    "OrderDetailUpdate"
 ]
