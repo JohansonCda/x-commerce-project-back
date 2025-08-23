a1aab177b3414324ab021035ce62c5829a7ceb78
santiago123x <santiago_123x@hotmail.com>
2025-08-23 16:34:57 -0500

    Add category routes: implement category namespace and CRUD operations for categories

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,10 +2,12 @@ from .image import images_ns
 from .main_routes import main_ns
 from .auth_routes import auth_ns
 from .product import products_ns
+from .category import categories_ns
 
 __all__ = [
     "images_ns",
     "main_ns",
     "auth_ns",
-    "products_ns"
+    "products_ns",
+    "categories_ns"
 ]
